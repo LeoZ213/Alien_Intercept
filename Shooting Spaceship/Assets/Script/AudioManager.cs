ab6d2c150fd648b597a623fc016567c792444dee
LeoZ213 <113804442+LeoZ213@users.noreply.github.com>
2023-10-14 17:21:50 -0400

    Background Music
    - Added background music to the main menu
    - Moved components to their correct folders

---
+++ Shooting Spaceship/Assets/Script/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioSource musicSource;
+
+    public AudioClip background;
+    public Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.clip = background;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    //Changes the volume of the music based on the slider value
+    public void ChangeVolume()
+    {
+        musicSource.volume = slider.value; 
+    }
+}
