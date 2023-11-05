b134c197036f0798322d201e5f7f03fead8eeee7
LeoZ213 <113804442+LeoZ213@users.noreply.github.com>
2023-11-05 17:24:17 -0500

    Powerup sprites, Level 03, GameManager
    - Added 3 powerup sprites (Extra life, Double bullets, Shield)
    - Made Level 03 (Not started)
    - Added ability for the next level to load when all enemies within a level are defeated
    - Added level 02 and level 03 to build settings

--- Shooting Spaceship/Assets/GameManager.cs
+++ Shooting Spaceship/Assets/GameManager.cs
@@ -6,6 +6,15 @@ public class GameManager : MonoBehaviour
 {
     bool hasGameEnded = false;
     float restartDelay = 1f;
+
+    private void Update()
+    {
+        int numberOfEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        if (numberOfEnemies <= 0)
+        {
+            Invoke("nextLevel", 1.0f);
+        }
+    }
     public void EndGame()
     {
         if (hasGameEnded == false)
@@ -18,5 +27,9 @@ public class GameManager : MonoBehaviour
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    void nextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
